e5531f63f279f58929f44a737d244e3ecbd7382e
Alexandra Chakeres <alexchakeres@gmail.com>
2019-06-25 16:58:18 -0600

    Add validation test for Merchant

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -5,4 +5,8 @@ RSpec.describe Merchant, type: :model do
     it { should have_many :invoices }
     it { should have_many :items }
   end
+
+  describe "validations" do
+    it { should validate_presence_of :name }
+  end
 end
