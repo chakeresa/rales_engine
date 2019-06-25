035cc5fccb3c2687333ef9eda7ba2168a4e03795
Alexandra Chakeres <alexchakeres@gmail.com>
2019-06-24 18:56:29 -0600

    Add 1 to many relationship (a merchant has_many items)

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
-  it { should have_many :invoices }
+  describe "relationships" do
+    it { should have_many :invoices }
+    it { should have_many :items }
+  end
 end
