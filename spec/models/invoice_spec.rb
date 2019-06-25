a6237c51879fb7f920c00cb109f8ba03fd62bce4
Alexandra Chakeres <alexchakeres@gmail.com>
2019-06-24 18:58:30 -0600

    Add 1 to many relationship (an invoice has_many transactions)

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Invoice, type: :model do
   describe "relationships" do
     it { should belong_to :customer }
     it { should belong_to :merchant }
-    xit { should have_many :transactions }
+    it { should have_many :transactions }
     xit { should have_many(:items).through(:invoice_items) }
   end
 end
