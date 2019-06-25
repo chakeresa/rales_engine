391687e770fb6bad0b1e0cd37f323061243b758b
Alexandra Chakeres <alexchakeres@gmail.com>
2019-06-25 11:32:49 -0600

    Add test data for CSV import

--- spec/services/csv_importer_spec.rb
+++ spec/services/csv_importer_spec.rb
@@ -2,8 +2,24 @@ require 'rails_helper'
 
 RSpec.describe CsvImporter, type: :service do
   describe "instance methods" do
-    xit "imports CSVs" do
-      #TO DO: how to test?
+    it "imports CSVs" do
+      tables = [["test_merchant", "Merchant", false],
+               ["test_item", "Item", false],
+               ["test_customer", "Customer", false],
+               ["test_invoice", "Invoice", true],
+               ["test_invoice_item", "InvoiceItem", true],
+               ["test_transaction", "Transaction", true],
+      ]
+      tables.each do |table|
+        CsvImporter.new(table).import
+      end
+
+      expect(Merchant.count).to eq(10)
+      expect(Item.count).to eq(9)
+      expect(Customer.count).to eq(5)
+      expect(Invoice.count).to eq(10)
+      expect(InvoiceItem.count).to eq(11)
+      expect(Transaction.count).to eq(12)
     end
   end
 end
