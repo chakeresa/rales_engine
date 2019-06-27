220bd022b56950f0a5714c787d8016280af50059
Alexandra Chakeres <alexchakeres@gmail.com>
2019-06-27 09:59:02 -0600

    Add ApplicationRecord::date_criteria helper method

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,13 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  private
+
+  def self.date_criteria(date)
+    if date
+      beginning_of_day = Time.zone.parse(date)
+      end_of_day = beginning_of_day + 1.days
+      {created_at: beginning_of_day..end_of_day}
+    end
+  end
 end
--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -6,17 +6,8 @@ class Invoice < ApplicationRecord
   has_many :items, through: :invoice_items
 
   def self.revenue_by_date(date = nil)
-    # TO DO: make helper method #date_criteria(date)
-    if date
-      beginning_of_day = Time.zone.parse(date)
-      end_of_day = beginning_of_day + 1.days
-      date_criteria = {created_at: beginning_of_day..end_of_day}
-    else
-      date_criteria = nil
-    end
-
     self.joins(:invoice_items, :transactions)
-        .where(date_criteria)
+        .where(date_criteria(date))
         .merge(Transaction.successful)
         .sum("invoice_items.quantity * invoice_items.unit_price")
   end
