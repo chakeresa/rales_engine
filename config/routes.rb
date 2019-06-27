39f73eef429c0132ee456fc1284b9b8b0a6216cf
Alexandra Chakeres <alexchakeres@gmail.com>
2019-06-27 13:39:43 -0600

    Add Merchants::FavoriteCustomerController/show endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
         get "/most_items", to: "most_items#index"
         get "/revenue", to: "revenue#index"
         get "/:id/revenue", to: "revenue#show"
+        get "/:id/favorite_customer", to: "favorite_customer#show"
       end
 
       resources :merchants, only: [:index, :show] do
