02d5cd3496cd1a0217f8d4a4c7248b5a770f68a4
Alexandra Chakeres <alexchakeres@gmail.com>
2019-06-27 17:17:08 -0600

    Refactor PriceSerializer -- change methods from instance to class

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
   def index
     revenue = Invoice.revenue_by_date(params[:date])
-    render json: PriceSerializer.new({"total_revenue": revenue}).render
+    render json: PriceSerializer.render({"total_revenue": revenue})
   end
 
   def show
     merchant_revenue = Merchant.find(params[:id]).revenue_by_date(params[:date])
-    render json: PriceSerializer.new({"revenue": merchant_revenue}).render
+    render json: PriceSerializer.render({"revenue": merchant_revenue})
   end
 end
--- app/serializers/price_serializer.rb
+++ app/serializers/price_serializer.rb
@@ -1,21 +1,17 @@
 class PriceSerializer < BaseSerializer
-  def initialize(attr_hash)
-    @attr_hash = attr_hash
-  end
-
-  def render
+  def self.render(attr_hash)
     {"data":
       {
-        attributes: formatted_attributes
+        attributes: formatted_attributes(attr_hash)
       }
     }.to_json
   end
 
   private
 
-  def formatted_attributes
-    @attr_hash.map do |key_value_ary|
-      [key_value_ary[0], PriceSerializer.format_price(key_value_ary[1])]
+  def self.formatted_attributes(attr_hash)
+    attr_hash.map do |key_value_ary|
+      [key_value_ary[0], format_price(key_value_ary[1])]
     end.to_h
   end
 end
