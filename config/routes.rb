893ea7e45a7422a129a5b1194f421c936f969ec9
Alexandra Chakeres <alexchakeres@gmail.com>
2019-06-28 10:44:47 -0600

    Add items/merchants_controllers#show

--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,7 @@ Rails.application.routes.draw do
       resources :items, only: [:index, :show] do
         scope module: :items do
           resources :invoice_items, only: [:index]
+          get "/merchant", to: "merchants#show"
         end
       end
     end
