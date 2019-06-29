7880bd0689173fc17f22df9d914ffb1b37fafb31
Alexandra Chakeres <alexchakeres@gmail.com>
2019-06-28 18:19:35 -0600

    Refactor Date/PriceSerializer - move ::render & ::formatted_attributes to BaseSerializer

--- app/serializers/date_serializer.rb
+++ app/serializers/date_serializer.rb
@@ -1,17 +1,7 @@
 class DateSerializer < BaseSerializer
-  def self.render(attr_hash)
-    {"data":
-      {
-        attributes: formatted_attributes(attr_hash)
-      }
-    }.to_json
-  end
-
   private
 
-  def self.formatted_attributes(attr_hash)
-    attr_hash.map do |key_value_ary|
-      [key_value_ary[0], key_value_ary[1].to_date.to_s]
-    end.to_h
+  def self.format_attr(attr)
+    attr.to_date.to_s
   end
 end
--- app/serializers/price_serializer.rb
+++ app/serializers/price_serializer.rb
@@ -1,17 +1,7 @@
 class PriceSerializer < BaseSerializer
-  def self.render(attr_hash)
-    {"data":
-      {
-        attributes: formatted_attributes(attr_hash)
-      }
-    }.to_json
-  end
-
   private
 
-  def self.formatted_attributes(attr_hash)
-    attr_hash.map do |key_value_ary|
-      [key_value_ary[0], format_price(key_value_ary[1])]
-    end.to_h
+  def self.format_attr(attr)
+    format_price(attr)
   end
 end
