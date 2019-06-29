54caf3cf1984d37ad1b49030b83e2994a6a6ad7f
Alexandra Chakeres <alexchakeres@gmail.com>
2019-06-29 09:54:53 -0600

    Add Invoices::RandomController#show

--- config/routes.rb
+++ config/routes.rb
@@ -54,7 +54,7 @@ Rails.application.routes.draw do
       namespace :invoices do
         get "/find", to: "search#show"
         get "/find_all", to: "search#index"
-        # get "/random", to: "random#show"
+        get "/random", to: "random#show"
       end
 
       resources :invoices, only: [:index, :show] do
